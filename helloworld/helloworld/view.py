9df728500b85e07b2422f9092ecf4535c505b34d
wfxu <296604998@qq.com>
2017-04-07 18:13:25 +0800

    rm helloworld

--- helloworld/helloworld/view.py
+++
@@ -1,8 +0,0 @@
-# -*- coding=utf-8 -*-
-#from django.http import HttpResponse
-from django.shortcuts import render
-
-def hello(request):
-	context={}
-	context['hello']='hello world! 世界和平'
-	return render(request,'hello.html',context)
